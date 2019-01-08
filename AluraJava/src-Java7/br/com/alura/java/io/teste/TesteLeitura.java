0e4608cd368c543fd0401563746334d6e3ec13fb
cleiton1992 <cl_nascto@hotmail.com>
2019-01-08 21:53:49 -0200

    Java IO e inserçao de arquivo txt

---
+++ AluraJava/src-Java7/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.alura.java.io.teste;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * @author Cleiton
+ *
+ */
+public class TesteLeitura {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//fluxo de entrada com arquivo	
+		FileInputStream fis = new FileInputStream("lorem.txt");
+
+	}
+
+}
